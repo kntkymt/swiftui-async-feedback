1b77f45982ea792fe2e3b9c08689064a9c3d5330
kntkymt <kntkc0de@gmail.com>
2024-04-30 19:02:26 +0900

    Add TestClock and TestClockTest

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "swiftui-async-feedback",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v11),
-        .tvOS(.v14),
-        .watchOS(.v7),
+        .iOS(.v16),
+        .macOS(.v13),
+        .tvOS(.v16),
+        .watchOS(.v9),
     ],
     products: [
         .library(
---
+++ Tests/AsyncFeedbackTestSupportTests/TestClockTests.swift
@@ -0,0 +1,169 @@
+import XCTest
+import AsyncFeedback
+@testable import AsyncFeedbackTestSupport
+
+final class TestClockTests: XCTestCase {
+
+    @MainActor
+    func testSchedule() async throws {
+        let clock = TestClock(initialInstant: .init())
+
+        var count = 0
+
+        // schedule
+        // id   : A B B C C
+        // clock: 0 1 2 3 4 5
+        // count: 0 1 1 3 3 6
+        let task = Task {
+            // clock: 0
+            try await clock.sleep(id: "A", for: .seconds(1))
+            count += 1
+
+            // clock: 1, 2
+            try await clock.sleep(id: "B", for: .seconds(2))
+            count += 2
+
+            // clock: 3, 4
+            try await clock.sleep(id: "C", until: .init(offset: .seconds(5)))
+            count += 3
+        }
+
+        //        ↓
+        // id   : A B B C C
+        // clock: 0 1 2 3 4 5
+        // count: 0 1 1 3 3 6
+        try await clock.sleep(untilSuspendBy: "A")
+        XCTAssertEqual(count, 0)
+
+        //          ↓
+        // id   : A B B C C
+        // clock: 0 1 2 3 4 5
+        // count: 0 1 1 3 3 6
+        clock.advance(by: .seconds(1))
+        try await clock.sleep(untilSuspendBy: "B")
+        XCTAssertEqual(count, 1)
+
+        //            ↓
+        // id   : A B B C C
+        // clock: 0 1 2 3 4 5
+        // count: 0 1 1 3 3 6
+        clock.advance(by: .seconds(1))
+        try await clock.sleep(untilSuspendBy: "B")
+        XCTAssertEqual(count, 1)
+
+        //              ↓
+        // id   : A B B C C
+        // clock: 0 1 2 3 4 5
+        // count: 0 1 1 3 3 6
+        clock.advance(by: .seconds(1))
+        try await clock.sleep(untilSuspendBy: "C")
+        XCTAssertEqual(count, 3)
+
+        //                    ↓
+        // id   : A B B C C
+        // clock: 0 1 2 3 4 5 6
+        // count: 0 1 1 3 3 6 6
+        clock.advance(by: .seconds(3))
+        try await task.value
+        XCTAssertEqual(count, 6)
+    }
+
+    @MainActor
+    func testResumeImmediate() async throws {
+        let clock = TestClock(initialInstant: .init())
+
+        var count = 0
+        //            ↓
+        // clock: 0 1 2
+        // count: 0 0 0
+        clock.advance(by: .seconds(2))
+
+        // schedule
+        // id   :   A B C
+        // clock: 0 1 2 3
+        // count: 0 1 3 6
+        let task = Task {
+            // clock: 0
+            try await clock.sleep(id: "A", until: .init(offset: .seconds(1)))
+            count += 1
+
+            try await clock.sleep(id: "B", until: .init(offset: .seconds(2)))
+            count += 2
+
+            // clock: 0
+            try await clock.sleep(id: "C", until: .init(offset: .seconds(3)))
+            count += 3
+        }
+
+        //            ↓
+        // id   : A B C
+        // clock: 0 1 2 3
+        // count: 0 0 3 6
+        try await clock.sleep(untilSuspendBy: "C")
+        XCTAssertEqual(count, 3)
+
+        //              ↓
+        // id   : A B C
+        // clock: 0 1 2 3
+        // count: 0 1 3 6
+        clock.advance(by: .seconds(1))
+        try await task.value
+        XCTAssertEqual(count, 6)
+    }
+
+    @MainActor
+    func testCancel() async throws {
+        let clock = TestClock(initialInstant: .init())
+
+        var count = 0
+
+        // schedule
+        // id   :   A
+        // clock: 0 1
+        // count: 0 1
+        let task = Task {
+            // clock: 0
+            try await clock.sleep(id: "A", until: .init(offset: .seconds(1)))
+            count += 1
+        }
+
+        try await clock.sleep(untilSuspendBy: "A")
+        task.cancel()
+        clock.advance(by: .seconds(1))
+
+        do {
+            try await task.value
+        } catch is CancellationError {
+            XCTAssertEqual(count, 0)
+        } catch {
+            XCTFail()
+        }
+    }
+
+    @MainActor
+    func testCancelImmediate() async throws {
+        let clock = TestClock(initialInstant: .init())
+
+        var count = 0
+
+        // schedule
+        // id   :   A
+        // clock: 0 1
+        // count: 0 1
+        let task = Task {
+            // clock: 0
+            try await clock.sleep(id: "A", until: .init(offset: .seconds(1)))
+            count += 1
+        }
+
+        task.cancel()
+
+        do {
+            try await task.value
+        } catch is CancellationError {
+            XCTAssertEqual(count, 0)
+        } catch {
+            XCTFail()
+        }
+    }
+}
